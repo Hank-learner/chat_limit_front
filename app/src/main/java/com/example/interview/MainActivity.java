473d644d19ceb9b56e7c4a9d24f289149d2d0a3e
shambu2k <sidharthshambu00@gmail.com>
2019-12-14 20:00:39 +0530

    login buttn

--- app/src/main/java/com/example/interview/MainActivity.java
+++ app/src/main/java/com/example/interview/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.interview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
 //    @BindView(R.id.editTextPassword)
 //    MaterialTextField editTextPassword;
 
+    @BindView(R.id.login_button)
+    Button Login_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
+        Login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChatListActivity.class));
+                overridePendingTransition  (R.anim.right_in, R.anim.right_out);
+            }
+        });
     }
 }
